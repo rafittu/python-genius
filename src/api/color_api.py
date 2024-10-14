b9ab3511db5e292059454e21b3855484f66a679f
rafittu <rafittu@outlook.com>
2024-10-14 13:46:48 -0300

    feat: setup websocket to receive random color

---
+++ src/api/color_api.py
@@ -0,0 +1,16 @@
+from flask_socketio import SocketIO, emit
+from flask import Blueprint
+
+color_api_bp = Blueprint('color_api', __name__)
+socketio = SocketIO(cors_allowed_origins="*")
+
+
+def init_sockets(app):
+    socketio.init_app(app)
+
+
+@socketio.on('send_color')
+def handle_receive_color(data):
+    color = data.get('color')
+    print(f"Received: {color}")
+    emit('Response', {'message': f"Color {color} received successfully!"})
