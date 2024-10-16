2efc6bcf995b72bbdcbcda7498a6b0f133c2c4e2
rafittu <rafittu@outlook.com>
2024-10-16 13:32:39 -0300

    feat: start database on app init

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from api.color_api import color_api_bp, init_sockets, socketio
+from database.database import init_app, db
 
 
 def create_app():
     app = Flask(__name__)
-
+    init_app(app)
     app.register_blueprint(color_api_bp)
-
     init_sockets(app)
 
+    with app.app_context():
+        db.create_all()
+    
     return app
 
 
