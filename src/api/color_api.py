12a25abfd6507098da0695d393b6a0c7e2870ef2
rafittu <rafittu@outlook.com>
2024-10-16 11:43:56 -0300

    feat: function to save color in database

--- src/api/color_api.py
+++ src/api/color_api.py
@@ -1,9 +1,13 @@
+import re
+import logging
 from flask_socketio import SocketIO
 from flask import Blueprint
-import re
+from src.database.models.color import Color
+from src.database.database import db
 
 color_api_bp = Blueprint('color_api', __name__)
 socketio = SocketIO(cors_allowed_origins="*")
+logger = logging.getLogger(__name__)
 
 COLOR_MAP = {
     "rgb(0, 0, 255)": "blue",
@@ -18,6 +22,10 @@ COLOR_MAP = {
 }
 
 
+def init_sockets(app):
+    socketio.init_app(app)
+
+
 def parse_rgb(color_str):
     match = re.match(r'rgb\((\d+),\s*(\d+),\s*(\d+)\)', color_str)
     if match:
@@ -29,13 +37,23 @@ def get_color_name(color_str):
     return COLOR_MAP.get(color_str, "unknown")
 
 
-def init_sockets(app):
-    socketio.init_app(app)
-
-
 @socketio.on('send_color')
 def handle_receive_color(data):
-    color = data.get('color')
+    color_str = data.get('color')
     timestamp = data.get('timestamp')
 
-    print(f"Received color: {color} at timestamp: {timestamp}")
+    try:
+        red, green, blue = parse_rgb(color_str)
+        color_name = get_color_name(color_str)
+        
+        new_color = Color(
+            color_name=color_name,
+            red=red,
+            green=green,
+            blue=blue,
+            timestamp=timestamp
+        )
+        db.session.add(new_color)
+        db.session.commit()
+    except ValueError as e:
+        logger.error(f"Error processing color: {e}")
